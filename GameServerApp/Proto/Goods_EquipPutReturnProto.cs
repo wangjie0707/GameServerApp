ff08cb29c04f3a7e8e888317f481f0a1caf0a3f5
ET <1364481911@qq.com>
2018-12-30 23:44:38 +0800

    重新提交

---
+++ GameServerApp/Proto/Goods_EquipPutReturnProto.cs
@@ -0,0 +1,63 @@
+﻿//===================================================
+//开 发 者：WY 
+//创建时间：2018-12-30 22:28:06
+//备    注：
+//===================================================
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// 服务器返回穿戴消息
+/// </summary>
+public struct Goods_EquipPutReturnProto : IProto
+{
+    public ushort ProtoCode { get { return 16013; } }
+    public string ProtoEnName { get { return "Goods_EquipPutReturn"; } }
+
+    public bool IsSuccess; //是否成功
+    public int MsgCode; //消息码
+    public byte Type; //0=穿上 1=脱下
+    public int GoodsId; //装备编号
+    public int GoodsServerId; //装备服务器端编号
+
+    public byte[] ToArray()
+    {
+        using (mmo_memotyStream ms = new mmo_memotyStream())
+        {
+            ms.WriteUShort(ProtoCode);
+            ms.WriteBool(IsSuccess);
+            if(IsSuccess)
+            {
+                ms.WriteByte(Type);
+                ms.WriteInt(GoodsId);
+                ms.WriteInt(GoodsServerId);
+            }
+            else
+            {
+            }
+            ms.WriteInt(MsgCode);
+            return ms.ToArray();
+        }
+    }
+
+    public static Goods_EquipPutReturnProto GetProto(byte[] buffer)
+    {
+        Goods_EquipPutReturnProto proto = new Goods_EquipPutReturnProto();
+        using (mmo_memotyStream ms = new mmo_memotyStream(buffer))
+        {
+            proto.IsSuccess = ms.ReadBool();
+            if(proto.IsSuccess)
+            {
+                proto.Type = (byte)ms.ReadByte();
+                proto.GoodsId = ms.ReadInt();
+                proto.GoodsServerId = ms.ReadInt();
+            }
+            else
+            {
+            }
+            proto.MsgCode = ms.ReadInt();
+        }
+        return proto;
+    }
+}
---
+++ GameServerApp/Proto/Goods_SellToSysReturnProto.cs
@@ -0,0 +1,48 @@
+﻿//===================================================
+//开 发 者：WY 
+//创建时间：2018-12-30 22:27:49
+//备    注：
+//===================================================
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// 服务器返回出售物品给系统消息
+/// </summary>
+public struct Goods_SellToSysReturnProto : IProto
+{
+    public ushort ProtoCode { get { return 16009; } }
+    public string ProtoEnName { get { return "Goods_SellToSysReturn"; } }
+
+    public bool IsSuccess; //是否成功
+    public int MsgCode; //消息码
+
+    public byte[] ToArray()
+    {
+        using (mmo_memotyStream ms = new mmo_memotyStream())
+        {
+            ms.WriteUShort(ProtoCode);
+            ms.WriteBool(IsSuccess);
+            if(!IsSuccess)
+            {
+            }
+            ms.WriteInt(MsgCode);
+            return ms.ToArray();
+        }
+    }
+
+    public static Goods_SellToSysReturnProto GetProto(byte[] buffer)
+    {
+        Goods_SellToSysReturnProto proto = new Goods_SellToSysReturnProto();
+        using (mmo_memotyStream ms = new mmo_memotyStream(buffer))
+        {
+            proto.IsSuccess = ms.ReadBool();
+            if(!proto.IsSuccess)
+            {
+            }
+            proto.MsgCode = ms.ReadInt();
+        }
+        return proto;
+    }
+}
